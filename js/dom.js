d2c3aac22953b7b1b51c7156bc371ec8be68ae89
edvicaty <edvicaty@hotmail.com>
2020-08-19 16:23:09 -0500

    final version 1.0

--- js/dom.js
+++ js/dom.js
@@ -1,93 +1,95 @@
-$body = document.querySelector('body');
-$domCanvas = document.querySelector('.canvas-dom');
+$body = document.querySelector("body");
+$domCanvas = document.querySelector(".canvas-dom");
 
 // Landing page:
-const $initiateDiv = document.createElement('div');
-$initiateDiv.classList.add('landing-div')
+const $initiateDiv = document.createElement("div");
+$initiateDiv.classList.add("landing-div");
 $domCanvas.appendChild($initiateDiv);
 
-const $startPhrase = document.createElement('h2');
-$startPhrase.classList.add('landing-subtitle')
-$startPhrase.innerText = 'Press the button below to start the game.';
+const $startPhrase = document.createElement("h2");
+$startPhrase.classList.add("landing-subtitle");
+$startPhrase.innerText = "Press the button below to start the game.";
 $initiateDiv.appendChild($startPhrase);
 
-const $initiateButton = document.createElement('button');
-$initiateButton.classList.add('landing-button')
-$initiateButton.innerText = 'Start';
-$initiateDiv.appendChild($initiateButton)
+const $initiateButton = document.createElement("button");
+$initiateButton.classList.add("landing-button");
+$initiateButton.innerText = "Start";
+$initiateDiv.appendChild($initiateButton);
 
 //2nd landing page: instructions
-$initiateButton.addEventListener('click', () => {
-    $initiateDiv.style.display = 'none'
-    loadInstructionPage();
-})
+$initiateButton.addEventListener("click", () => {
+  startMusic.play();
 
-const $instructionsDiv = document.createElement('div');
-$instructionsDiv.classList.add('landing-div')
+  $initiateDiv.style.display = "none";
+  loadInstructionPage();
+});
+
+const $instructionsDiv = document.createElement("div");
+$instructionsDiv.classList.add("landing-div");
 
 function loadInstructionPage() {
-    $domCanvas.appendChild($instructionsDiv);
+  $domCanvas.appendChild($instructionsDiv);
 }
 
-const $instructions = document.createElement('h2');
-$instructions.classList.add('landing-subtitle')
-$instructions.innerText = 'Game instructions:';
+const $instructions = document.createElement("h2");
+$instructions.classList.add("landing-subtitle");
+$instructions.innerText = "Game instructions:";
 $instructionsDiv.appendChild($instructions);
-$instructions.style.fontSize = '2.4rem'
-$instructions.style.margin = '0px'
+$instructions.style.fontSize = "2.4rem";
+$instructions.style.margin = "0px";
 
-$instructionsList = document.createElement('ul')
-$instructionsList.style.marginTop = '0px'
-$instructionsList.classList.add('instruction-list')
+$instructionsList = document.createElement("ul");
+$instructionsList.style.marginTop = "0px";
+$instructionsList.classList.add("instruction-list");
 $instructionsList.innerHTML = `
 <li>Physical distancing: Dodge the virus <img src='../img/objects/covid.png'></li>
 <li>Use face masks: Grab the falling face masks for protection <img src='../img/objects/cubrebocas.png'></li>
 <li>Find the cure: Grab the spawning syringes in order to win <img src='../img/objects/seringeDef.png'><br>points and complete the level</li>
 <li>Tips: Use the platforms to reach your goal <img src='../img/bg/brick-platform.png'><br>and use the arrow keys of your keyboard to move <img src='../img/objects/arrow-keys.png'></li>
-`
-$instructionsDiv.appendChild($instructionsList)
+`;
+$instructionsDiv.appendChild($instructionsList);
 
-const $nextPageButton = document.createElement('button');
-$nextPageButton.classList.add('landing-button')
-$nextPageButton.style.display = 'inline'
-$nextPageButton.style.fontSize = '1.4rem'
-$nextPageButton.style.width = '260px'
-$nextPageButton.style.marginTop = '0px'
-$nextPageButton.innerText = 'Choose your character'
-$instructionsDiv.appendChild($nextPageButton)
+const $nextPageButton = document.createElement("button");
+$nextPageButton.classList.add("landing-button");
+$nextPageButton.style.display = "inline";
+$nextPageButton.style.fontSize = "1.4rem";
+$nextPageButton.style.width = "260px";
+$nextPageButton.style.marginTop = "0px";
+$nextPageButton.innerText = "Choose your character";
+$instructionsDiv.appendChild($nextPageButton);
 
 // 3rd landing page: choose your character
-$nextPageButton.addEventListener('click', () => {
-    $instructionsDiv.style.display = 'none'
-    loadCharacterPage();
-})
+$nextPageButton.addEventListener("click", () => {
+  $instructionsDiv.style.display = "none";
+  loadCharacterPage();
+});
 
-const $charactersDiv = document.createElement('div');
-$charactersDiv.classList.add('landing-div')
+const $charactersDiv = document.createElement("div");
+$charactersDiv.classList.add("landing-div");
 
 function loadCharacterPage() {
-    $domCanvas.appendChild($charactersDiv);
+  $domCanvas.appendChild($charactersDiv);
 }
 
-const $charactersDivTitle = document.createElement('h2')
-$charactersDivTitle.classList.add('landing-subtitle')
-$charactersDivTitle.innerText = 'Choose your character:'
-$charactersDivTitle.style.marginBottom = '0px'
-$charactersDiv.appendChild($charactersDivTitle)
+const $charactersDivTitle = document.createElement("h2");
+$charactersDivTitle.classList.add("landing-subtitle");
+$charactersDivTitle.innerText = "Choose your character:";
+$charactersDivTitle.style.marginBottom = "0px";
+$charactersDiv.appendChild($charactersDivTitle);
 
-const $characterButtons = []
-const $charactersFacesDiv = document.createElement('div');
-$charactersFacesDiv.classList.add('faces')
-const characters = [trump, macron, putin, merkel, johnson, jinping, amlo]
+const $characterButtons = [];
+const $charactersFacesDiv = document.createElement("div");
+$charactersFacesDiv.classList.add("faces");
+const characters = [trump, macron, putin, merkel, johnson, jinping, amlo];
 characters.forEach((character, index) => {
-    $charactersFacesDiv.innerHTML += `
+  $charactersFacesDiv.innerHTML += `
     <div class="character-card card-${index}">
     <img src='${character.img}'>
     <h3>${character.name}</h3>
     </div>
-    `
-})
-$charactersDiv.appendChild($charactersFacesDiv)
+    `;
+});
+$charactersDiv.appendChild($charactersFacesDiv);
 
 // const $characterCards = document.querySelectorAll('.character-card')
 // $characterCards.forEach(character => {
@@ -95,123 +97,128 @@ $charactersDiv.appendChild($charactersFacesDiv)
 // })
 
 // Select character
-$charactersFacesDiv.addEventListener('click', event => {
-    switch (event.path[0].nextElementSibling.innerText) {
-        case 'Donald Trump':
-            chosenCharacter = trump;
-            setCharacter(trump);
-            break;
-        case 'Emmanuel Macron':
-            chosenCharacter = macron;
-            setCharacter(macron);
-            break;
-        case 'Vladimir Putin':
-            chosenCharacter = putin;
-            setCharacter(putin);
-            break;
-        case 'Angela Merkel':
-            chosenCharacter = merkel;
-            setCharacter(merkel);
-            break;
-        case 'Boris Johnson':
-            chosenCharacter = johnson;
-            setCharacter(johnson);
-            break;
-        case 'Xi Jinping':
-            chosenCharacter = jinping;
-            setCharacter(jinping);
-            break;
-        case 'Andrés Manuel López Obrador':
-            chosenCharacter = amlo;
-            setCharacter(amlo);
-            break;
-    }
-    $charactersDiv.style.display = 'none'
-    loadStartingPage();
-})
+$charactersFacesDiv.addEventListener("click", (event) => {
+  switch (event.path[0].nextElementSibling.innerText) {
+    case "Donald Trump":
+      chosenCharacter = trump;
+      setCharacter(trump);
+      break;
+    case "Emmanuel Macron":
+      chosenCharacter = macron;
+      setCharacter(macron);
+      break;
+    case "Vladimir Putin":
+      chosenCharacter = putin;
+      setCharacter(putin);
+      break;
+    case "Angela Merkel":
+      chosenCharacter = merkel;
+      setCharacter(merkel);
+      break;
+    case "Boris Johnson":
+      chosenCharacter = johnson;
+      setCharacter(johnson);
+      break;
+    case "Xi Jinping":
+      chosenCharacter = jinping;
+      setCharacter(jinping);
+      break;
+    case "Andrés Manuel López Obrador":
+      chosenCharacter = amlo;
+      setCharacter(amlo);
+      break;
+  }
+  $charactersDiv.style.display = "none";
+  loadStartingPage();
+});
 
 // 4th landing page: Start button
-const $startingPage = document.createElement('div');
-$startingPage.classList.add('landing-div')
+const $startingPage = document.createElement("div");
+$startingPage.classList.add("landing-div");
 
 function loadStartingPage() {
-    $domCanvas.appendChild($startingPage);
+  $domCanvas.appendChild($startingPage);
 
-    const $chosenCharacterDiv = document.createElement('div')
-    $chosenCharacterDiv.classList.add('ready-to-play')
-    $chosenCharacterDiv.style.color = 'white'
-    $chosenCharacterDiv.style.fontSize = '1.3rem'
-    $chosenCharacterDiv.innerHTML = `
+  const $chosenCharacterDiv = document.createElement("div");
+  $chosenCharacterDiv.classList.add("ready-to-play");
+  $chosenCharacterDiv.style.color = "white";
+  $chosenCharacterDiv.style.fontSize = "1.3rem";
+  $chosenCharacterDiv.innerHTML = `
     <h2>You chose to play with: ${chosenCharacter.name}</h2>
     <img src="${chosenCharacter.img}">
-    `
-    $startingPage.appendChild($chosenCharacterDiv);
+    `;
+  $startingPage.appendChild($chosenCharacterDiv);
 }
 
-const $startButton = document.createElement('button')
-$startButton.classList.add('landing-button')
-$startButton.innerText = 'Start Game'
-$startButton.style.marginTop = '40px'
-$startingPage.appendChild($startButton)
+const $startButton = document.createElement("button");
+$startButton.classList.add("landing-button");
+$startButton.innerText = "Start Game";
+$startButton.style.marginTop = "40px";
+$startingPage.appendChild($startButton);
 
 // Event listener: Start game
-const $pauseButton = document.createElement('button')
-$pauseButton.classList.add('pause-btn')
-$pauseButton.style.display = 'none';
+
+const $pauseButton = document.createElement("button");
+$pauseButton.classList.add("pause-btn");
+$pauseButton.style.display = "none";
 $pauseButton.innerHTML = `
 <img src='../img/icons/pause.png'>
 <span>PAUSE GAME</span>
-`
-$body.appendChild($pauseButton)
+`;
+$body.appendChild($pauseButton);
 
-$startButton.addEventListener('click', () => {
-    $domCanvas.style.display = 'none'
-    $canvas.style.display = 'block'
-    startGame();
-})
+$startButton.addEventListener("click", () => {
+  startMusic.pause();
+  generalMusic.play();
+  backgroundSound.play();
+  $domCanvas.style.display = "none";
+  $canvas.style.display = "block";
+  startGame();
+});
 
 // Pause event
-$pauseButton.addEventListener('click', () => {
-    const $pauseOrResume = $pauseButton.querySelector('span');
-    const $pauseOrResumeImg = $pauseButton.querySelector('img')
-    let pauseOrResumeText = $pauseOrResume.innerText;
+$pauseButton.addEventListener("click", () => {
+  const $pauseOrResume = $pauseButton.querySelector("span");
+  const $pauseOrResumeImg = $pauseButton.querySelector("img");
+  let pauseOrResumeText = $pauseOrResume.innerText;
 
-    if (pauseOrResumeText === 'PAUSE GAME') {
-        $pauseOrResume.innerText = 'RESUME GAME'
-        $pauseOrResumeImg.src = '../img/icons/play.png'
-        pauseGame()
-    } else if (pauseOrResumeText === 'RESUME GAME') {
-        $pauseOrResume.innerText = 'PAUSE GAME'
-        $pauseOrResumeImg.src = '../img/icons/pause.png'
-        startGame()
-    }
-})
-
+  if (pauseOrResumeText === "PAUSE GAME") {
+    $pauseOrResume.innerText = "RESUME GAME";
+    $pauseOrResumeImg.src = "../img/icons/play.png";
+    backgroundSound.pause();
+    pauseGame();
+  } else if (pauseOrResumeText === "RESUME GAME") {
+    $pauseOrResume.innerText = "PAUSE GAME";
+    $pauseOrResumeImg.src = "../img/icons/pause.png";
+    backgroundSound.play();
+    startGame();
+  }
+});
 
 // Next level button
-const $nextButton = document.createElement('button');
-$nextButton.classList.add('pause-btn');
-$nextButton.style.display = 'none';
+const $nextButton = document.createElement("button");
+$nextButton.classList.add("pause-btn");
+$nextButton.style.display = "none";
 $nextButton.innerHTML = `
 <img src='../img/icons/play.png'>
 <span>NEXT LEVEL</span>
-`
-$body.appendChild($nextButton)
+`;
+$body.appendChild($nextButton);
 
-$nextButton.addEventListener('click', () => {
-    addLevel()
-})
+$nextButton.addEventListener("click", () => {
+  addLevel();
+});
 
 // Restart game when gameOver
-const $restartButton = document.createElement('button');
-$restartButton.classList.add('pause-btn');
-$restartButton.style.display = 'none';
+const $restartButton = document.createElement("button");
+$restartButton.classList.add("pause-btn");
+$restartButton.style.display = "none";
 $restartButton.innerHTML = `
 <img src='../img/icons/play.png'>
 <span>RESTART GAME</span>
-`
-$body.appendChild($restartButton)
+`;
+$body.appendChild($restartButton);
 
-$restartButton.addEventListener('click', () => {
-    window.location.reload()
-})
+$restartButton.addEventListener("click", () => {
+  window.location.reload();
+});
--- js/variables.js
+++ js/variables.js
@@ -1,6 +1,27 @@
 const $canvas = document.querySelector("canvas");
 const ctx = $canvas.getContext("2d");
 
+//sounds;
+const startMusic = new Audio("../sounds/intro-music-1.mp3");
+startMusic.loop = true;
+startMusic.volume = 0.5;
+const generalMusic = new Audio("../sounds/general-level-music.mp3");
+generalMusic.loop = true;
+generalMusic.volume = 0.4;
+const lastLevelMusic = new Audio("../sounds/level-5-music-1.mp3");
+lastLevelMusic.loop = true;
+lastLevelMusic.volume = 0.5;
+const backgroundSound = new Audio("../sounds/general-background.mp3");
+backgroundSound.volume = 0.2;
+backgroundSound.loop = true;
+const smallWin = new Audio("../sounds/small-win.mp3");
+const bigWin = new Audio("../sounds/big-win.mp3");
+const lose = new Audio("../sounds/lose.mp3");
+const collectSyringe = new Audio("../sounds/collect-syringe.mp3");
+const collectMask = new Audio("../sounds/collect-mask.mp3");
+const virusHit = new Audio("../sounds/virus-hit.mp3");
+const fierroViejo = new Audio("../sounds/fierro-viejo.mp3");
+
 let frames = 0;
 const gravity = 0.98;
 const friction = 0.8;
@@ -13,95 +34,95 @@ let seringeApparition = 500;
 let currentLevel = 1;
 
 const amlo = {
-    name: "Andrés Manuel López Obrador",
-    gender: "he",
-    img: "../img/characters/amlo.png",
-    offsetX: 11,
-    offsetY: -35,
-    ratioWidth: 1 / 1.3,
-    ratioHeight: 1 / 1.7,
-    offsetMaskX: 19,
-    offsetMaskY: 7,
-    ratioMaskWidth: 1 / 1.4,
-    ratioMaskHeight: 1 / 3,
+  name: "Andrés Manuel López Obrador",
+  gender: "he",
+  img: "../img/characters/amlo.png",
+  offsetX: 11,
+  offsetY: -35,
+  ratioWidth: 1 / 1.3,
+  ratioHeight: 1 / 1.7,
+  offsetMaskX: 19,
+  offsetMaskY: 7,
+  ratioMaskWidth: 1 / 1.4,
+  ratioMaskHeight: 1 / 3,
 };
 const trump = {
-    name: "Donald Trump",
-    gender: "he",
-    img: "../img/characters/trump.png",
-    offsetX: 19,
-    offsetY: -30,
-    ratioWidth: 1 / 1.5,
-    ratioHeight: 1 / 1.8,
-    offsetMaskX: 20,
-    offsetMaskY: 10,
-    ratioMaskWidth: 1 / 1.5,
-    ratioMaskHeight: 1 / 3.2,
+  name: "Donald Trump",
+  gender: "he",
+  img: "../img/characters/trump.png",
+  offsetX: 19,
+  offsetY: -30,
+  ratioWidth: 1 / 1.5,
+  ratioHeight: 1 / 1.8,
+  offsetMaskX: 20,
+  offsetMaskY: 10,
+  ratioMaskWidth: 1 / 1.5,
+  ratioMaskHeight: 1 / 3.2,
 };
 const macron = {
-    name: "Emmanuel Macron",
-    gender: "he",
-    img: "../img/characters/macron.png",
-    offsetX: 19,
-    offsetY: -30,
-    ratioWidth: 1 / 1.5,
-    ratioHeight: 1 / 1.8,
-    offsetMaskX: 16,
-    offsetMaskY: 12,
-    ratioMaskWidth: 1 / 1.4,
-    ratioMaskHeight: 1 / 3.2,
+  name: "Emmanuel Macron",
+  gender: "he",
+  img: "../img/characters/macron.png",
+  offsetX: 19,
+  offsetY: -30,
+  ratioWidth: 1 / 1.5,
+  ratioHeight: 1 / 1.8,
+  offsetMaskX: 16,
+  offsetMaskY: 12,
+  ratioMaskWidth: 1 / 1.4,
+  ratioMaskHeight: 1 / 3.2,
 };
 const putin = {
-    name: "Vladimir Putin",
-    gender: "he",
-    img: "../img/characters/putin.png",
-    offsetX: 11,
-    offsetY: -30,
-    ratioWidth: 1 / 1.4,
-    ratioHeight: 1 / 1.8,
-    offsetMaskX: 16,
-    offsetMaskY: 8,
-    ratioMaskWidth: 1 / 1.4,
-    ratioMaskHeight: 1 / 3.2,
+  name: "Vladimir Putin",
+  gender: "he",
+  img: "../img/characters/putin.png",
+  offsetX: 11,
+  offsetY: -30,
+  ratioWidth: 1 / 1.4,
+  ratioHeight: 1 / 1.8,
+  offsetMaskX: 16,
+  offsetMaskY: 8,
+  ratioMaskWidth: 1 / 1.4,
+  ratioMaskHeight: 1 / 3.2,
 };
 const merkel = {
-    name: "Angela Merkel",
-    gender: "she",
-    img: "../img/characters/merkel.png",
-    offsetX: 15,
-    offsetY: -32,
-    ratioWidth: 1 / 1.4,
-    ratioHeight: 1 / 1.8,
-    offsetMaskX: 16,
-    offsetMaskY: 8,
-    ratioMaskWidth: 1 / 1.4,
-    ratioMaskHeight: 1 / 3.2,
+  name: "Angela Merkel",
+  gender: "she",
+  img: "../img/characters/merkel.png",
+  offsetX: 15,
+  offsetY: -32,
+  ratioWidth: 1 / 1.4,
+  ratioHeight: 1 / 1.8,
+  offsetMaskX: 16,
+  offsetMaskY: 8,
+  ratioMaskWidth: 1 / 1.4,
+  ratioMaskHeight: 1 / 3.2,
 };
 const johnson = {
-    name: "Boris Johnson",
-    gender: "he",
-    img: "../img/characters/johnson.png",
-    offsetX: 16,
-    offsetY: -34,
-    ratioWidth: 1 / 1.3,
-    ratioHeight: 1 / 1.8,
-    offsetMaskX: 19,
-    offsetMaskY: 12,
-    ratioMaskWidth: 1 / 1.5,
-    ratioMaskHeight: 1 / 3.3,
+  name: "Boris Johnson",
+  gender: "he",
+  img: "../img/characters/johnson.png",
+  offsetX: 16,
+  offsetY: -34,
+  ratioWidth: 1 / 1.3,
+  ratioHeight: 1 / 1.8,
+  offsetMaskX: 19,
+  offsetMaskY: 12,
+  ratioMaskWidth: 1 / 1.5,
+  ratioMaskHeight: 1 / 3.3,
 };
 const jinping = {
-    name: "Xi Jinping",
-    gender: "he",
-    img: "../img/characters/jinping.png",
-    offsetX: 20,
-    offsetY: -32,
-    ratioWidth: 1 / 1.7,
-    ratioHeight: 1 / 1.9,
-    offsetMaskX: 8,
-    offsetMaskY: 5,
-    ratioMaskWidth: 1 / 1.2,
-    ratioMaskHeight: 1 / 3.2,
+  name: "Xi Jinping",
+  gender: "he",
+  img: "../img/characters/jinping.png",
+  offsetX: 20,
+  offsetY: -32,
+  ratioWidth: 1 / 1.7,
+  ratioHeight: 1 / 1.9,
+  offsetMaskX: 8,
+  offsetMaskY: 5,
+  ratioMaskWidth: 1 / 1.2,
+  ratioMaskHeight: 1 / 3.2,
 };
 
 let chosenCharacter = amlo;
